9df4dc85abc22ee99135d8a8146178317e73d7fd
Ayo <talktojmcvibes@gmail.com>
2024-05-06 22:33:18 +0100

    connect to db, receive data and save to db

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require("dotenv").config()
 
 
 
-const app = express();
+const app = express(); 
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+const PORT = process.env.PORT || 3000;
+const mongoURL = process.env.DB
+mongoose.connect(mongoURL)
+.then(
+    console.log("Connected to database")
+).catch((err)=>{
+    console.log(`Error connecting to database ${err}`)
+});
+
+const taskSchema = mongoose.Schema({
+    task: {
+        type: String,
+        require: true
+    }
+});
+
+const task = mongoose.model("tasks", taskSchema);
 
 
 app.get('/', (req, res)=>{
     res.send("Hello world!")
 });
 
+app.post('/', (req, res)=>{
+    newTask = req.body.task;
+    try{
+        const saveTask = new task({
+            task: newTask
+        })
+        saveTask.save()
+        res.send({Message: "Task added successfully"})
+    } catch(err){
+        console.log(`Error Adding new task ${err}`)
+    }
 
-app.listen(3000, ()=>{
-    console.log("App listening on port 3000")
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`App listening on port ${PORT}`)
 })
